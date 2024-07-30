4137e6614db0d30144f379e3f9a66b1d19ae476e
palakpadalia <palak@sanskartechnolab.com>
2024-07-30 16:56:44 +0530

    Item API with Filters

--- agility/public/api/item.py
+++ agility/public/api/item.py
@@ -2,20 +2,26 @@ import frappe
 from frappe import _
 
 
-
 @frappe.whitelist()
-def item_wise_assets():
+def item_wise_assets(
+    filters={},
+    fields=["*"],
+    limit_start=0,
+    limit_page_length=20,
+    order_by="modified desc",
+):
     items = frappe.db.get_list(
         "Item",
-        filters={},
-        fields=["*"],
-        # order_by="name",
-        limit=None,
+        filters=frappe.parse_json(filters),
+        fields=frappe.parse_json(fields),
+        limit_start=limit_start,
+        limit_page_length=limit_page_length,
+        order_by=order_by,
     )
 
     #! GET CHILD DOCTYPE LIST
     custom_assets_list = frappe.db.get_all(
-        "Product Images - Child",
+        "Product Image - Child",
         filters={"parenttype": "Item"},
         fields=["*"],
         order_by="parent",
@@ -38,16 +44,18 @@ def item_wise_assets():
     return items
 
 
-
-
 @frappe.whitelist()
 def get_color_code(item_attribute, template_code):
-    data = frappe.db.sql(" select attribute_value from `tabItem Variant Attribute` where variant_of=%s and attribute=%s ", (template_code, item_attribute), as_dict = True)
+    data = frappe.db.sql(
+        " select attribute_value from `tabItem Variant Attribute` where variant_of=%s and attribute=%s ",
+        (template_code, item_attribute),
+        as_dict=True,
+    )
 
     # Use a set to remove duplicates
-    unique_attribute_values = list({v['attribute_value'] for v in data})
+    unique_attribute_values = list({v["attribute_value"] for v in data})
 
     # Convert back to list of dictionaries if necessary
-    unique_attribute_values = [{'attribute_value': v} for v in unique_attribute_values]
+    unique_attribute_values = [{"attribute_value": v} for v in unique_attribute_values]
 
     return unique_attribute_values
