5112e657db5615c285b6b6013109e604dafd4dbb
palakpadalia <palak@sanskartechnolab.com>
2024-08-28 12:10:54 +0530

    item master fields delete and opeastion set property setter set

--- agility/hooks.py
+++ agility/hooks.py
@@ -231,3 +231,7 @@ doc_events = {
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
+fixtures = [    
+    
+    {"dt": "Property Setter", "filters": [["module", "in", "Agility"]]},
+]
